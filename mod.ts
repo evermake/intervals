05e954eede67ac9bea0f23c72d6e1edf9011840f
evermake <vladeryabkin@gmail.com>
2025-04-16 17:44:37 +0500

    release: v0.2.0

--- mod.ts
+++ mod.ts
@@ -1,9 +1,10 @@
 /**
  * Library that provides utilities for working with intervals.
- * 
+ *
  * @module
  */
 
 export * from "./interval.ts";
 export * from "./merge.ts";
 export * from "./exclude.ts";
+export * from "./includes.ts";
